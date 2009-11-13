1f57774e2150a1648c7e532c6c805f9f47442eaa
banane <banane@gmail.com>
2009-11-13 14:34:52 -0800

    fixed admin tab controller

--- app/controllers/mediamaid_controller.rb
+++ app/controllers/mediamaid_controller.rb
@@ -1,10 +1,10 @@
-class MediaController < ApplicationController
+class MediamaidController  < ApplicationController
   # GET /Medias
   # GET /Medias.xml
   def index
     @medias = Media.all
 
-    respond_to do |format|
+    respond_tdControllero do |format|
       format.html # index.html.erb
       format.xml  { render :xml => @medias }
     end
