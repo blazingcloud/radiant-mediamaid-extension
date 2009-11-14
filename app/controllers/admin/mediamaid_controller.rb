0a9ffab10eba5331bab7067e1c419bdec0cefd36
Liah Hansen <liahhansen@gmail.com>
2009-11-13 18:07:29 -0800

    debugging needed for destroy

--- app/controllers/admin/mediamaid_controller.rb
+++ app/controllers/admin/mediamaid_controller.rb
@@ -1,22 +1,12 @@
 class Admin::MediamaidController  < ApplicationController
-  # GET /Medias
-  # GET /Medias.xml
   def index
-    @mediamaids = Mediamaid.all
-   
-    # respond_to do |format|
-    #   format.html # index.html.erb
-    #   format.xml  { render :xml => @mediamaids }
-    # end
-  end
-  
-  # GET /Medias/new
-  # GET /Medias/new.xml
+    @mediamaids = Mediamaid.all 
+  end 
+
   def new
     @mediamaid = Mediamaid.new
   end
-  # POST /Medias
-  # POST /Medias.xml
+
   def create 
     @mediamaid = Mediamaid.create( params[:mediamaid] )
  
@@ -32,4 +22,29 @@ class Admin::MediamaidController  < ApplicationController
     end
   end
   
+  def destroy
+    
+    @mediamaid = Mediamaid.find(params[:id])
+    @mediamaid.destroy
+
+    # respond_to do |format|
+    #   format.html { redirect_to(Mediamaids_url) }
+    #   format.xml  { head :ok }
+    # end
+  end
+  
+  # def update
+  #   @media = Media.find(params[:id])
+  # 
+  #   respond_to do |format|
+  #     if @media.update_attributes(params[:Media])
+  #       flash[:notice] = 'Media was successfully updated.'
+  #       format.html { redirect_to(@media) }
+  #       format.xml  { head :ok }
+  #     else
+  #       format.html { render :action => "edit" }
+  #       format.xml  { render :xml => @media.errors, :status => :unprocessable_entity }
+  #     end
+  #   end
+  # end
 end
