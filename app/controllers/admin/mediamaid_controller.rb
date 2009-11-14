5ac0573a6719f4411d1ed4a126b8f749ff0d91e2
Liah Hansen <liahhansen@gmail.com>
2009-11-13 16:03:34 -0800

    FIXED the template problem. woo.

---
+++ app/controllers/admin/mediamaid_controller.rb
@@ -0,0 +1,13 @@
+class Admin::MediamaidController  < ApplicationController
+  # GET /Medias
+  # GET /Medias.xml
+  def index
+    @mediamaids = Mediamaid.all
+   
+    # respond_to do |format|
+    #   format.html # index.html.erb
+    #   format.xml  { render :xml => @mediamaids }
+    # end
+  end
+
+end
