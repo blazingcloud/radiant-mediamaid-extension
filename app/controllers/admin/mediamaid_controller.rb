0f2daa7441b72dfbe4c24c6f8cf74e2ab472437d
banane <banane@gmail.com>
2009-11-14 00:38:24 -0800

    fixed create and show views

--- app/controllers/admin/mediamaid_controller.rb
+++ app/controllers/admin/mediamaid_controller.rb
@@ -7,44 +7,21 @@ class Admin::MediamaidController  < ApplicationController
     @mediamaid = Mediamaid.new
   end
 
-  def create 
+   def create 
     @mediamaid = Mediamaid.create( params[:mediamaid] )
- 
     respond_to do |format|
-      if @mediamaid.save
-        flash[:notice] = 'Mediamaid was successfully created.'
-        format.html { redirect_to(@mediamaid) }
-        format.xml  { render :xml => @mediamaid, :status => :created, :location => @mediamaid }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @mediamaid.errors, :status => :unprocessable_entity }
-      end
+       flash[:notice] = 'File was successfully loaded!'
+       format.html { render :action => "show" }
     end
   end
   
-  def destroy
-    
-    @mediamaid = Mediamaid.find(params[:id])
-    @mediamaid.destroy
-
-    # respond_to do |format|
-    #   format.html { redirect_to(Mediamaids_url) }
-    #   format.xml  { head :ok }
-    # end
-  end
   
-  # def update
-  #   @media = Media.find(params[:id])
-  # 
-  #   respond_to do |format|
-  #     if @media.update_attributes(params[:Media])
-  #       flash[:notice] = 'Media was successfully updated.'
-  #       format.html { redirect_to(@media) }
-  #       format.xml  { head :ok }
-  #     else
-  #       format.html { render :action => "edit" }
-  #       format.xml  { render :xml => @media.errors, :status => :unprocessable_entity }
-  #     end
-  #   end
-  # end
+  def show
+    @mediamaid = Mediamaid.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @mediamaid }
+    end
+  end
 end
