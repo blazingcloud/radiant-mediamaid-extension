856e2efd156c35afcaaca3a567116c4d862cd89a
Liah Hansen <liahhansen@gmail.com>
2009-11-18 11:41:32 -0800

    resolving another conflict

--- app/controllers/admin/mediamaid_controller.rb
+++ app/controllers/admin/mediamaid_controller.rb
@@ -8,54 +8,7 @@ class Admin::MediamaidController  < ApplicationController
     @mediamaid = Mediamaid.new
   end
 
-<<<<<<< HEAD
-   # def create 
-   #  @mediamaid = Mediamaid.create( params[:mediamaid] )
-   #  render :layout => false
-   #  if (request.xhr?)
-   #    flash[:notice] = 'Was an Ajax call.'
-   #  else
-   #    flash[:notice] = 'Was not an Ajax call.'
-   #  end
-   # respond_to do |format|
-  #   flash[:notice] = 'File was successfully loaded!'   
-   #  format.html { render :action => "show" }
-   # end
-    # end
-   
-   def create
-     @mediamaid = Mediamaid.new(params[:mediamaid])
-     respond_to do |format|
-       if @mediamaid.save
-         flash[:notice] = 'Mediamaid was successfully created.'
-         format.html { redirect_to mediamaid_url(@mediamaid) }
-         format.xml  { head :created, :location => mediamaid_url(@mediamaid) }
-         format.js do
-           responds_to_parent do
-             render :update do |page|
-               page.insert_html :bottom, "mediamaids", :partial => 'mediamaids/list_item', :object => @mediamaid
-               page.visual_effect :highlight, "mediamaid_#{@mediamaid.id}" 
-             end
-           end          
-         end
-       else
-         format.html { render :action => "new" }
-         format.xml  { render :xml => @mediamaid.errors.to_xml }
-         format.js do
-           responds_to_parent do
-             render :update do |page|
-                 # update the page with an error message
-             end
-           end          
-         end
-       end
-     end
-   end
-   
-   
-   
- 
-=======
+
    def create 
     @mediamaid = Mediamaid.create( params[:mediamaid] )
     respond_to do |format|
@@ -63,7 +16,6 @@ class Admin::MediamaidController  < ApplicationController
        format.html { render :action => "show" }
      end
   end
->>>>>>> 57ea9dca85215e44b685d7731c5564dc456a2a1c
   
   
   def show
