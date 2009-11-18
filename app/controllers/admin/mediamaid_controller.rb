f1dd391c6d7415c1f73fd188c8d94cb9c2f07eca
Liah Hansen <liahhansen@gmail.com>
2009-11-17 18:58:29 -0800

    version of demo from ajax blog directions.  doesnt work yet completely.

--- app/controllers/admin/mediamaid_controller.rb
+++ app/controllers/admin/mediamaid_controller.rb
@@ -9,19 +9,52 @@ class Admin::MediamaidController  < ApplicationController
     @mediamaid = Mediamaid.new
   end
 
-   def create 
-    @mediamaid = Mediamaid.create( params[:mediamaid] )
-    render :layout => false
-    if (request.xhr?)
-      flash[:notice] = 'Was an Ajax call.'
-    else
-      flash[:notice] = 'Was not an Ajax call.'
-    end
+   # def create 
+   #  @mediamaid = Mediamaid.create( params[:mediamaid] )
+   #  render :layout => false
+   #  if (request.xhr?)
+   #    flash[:notice] = 'Was an Ajax call.'
+   #  else
+   #    flash[:notice] = 'Was not an Ajax call.'
+   #  end
    # respond_to do |format|
   #   flash[:notice] = 'File was successfully loaded!'   
    #  format.html { render :action => "show" }
    # end
-  end
+    # end
+   
+   def create
+     @mediamaid = Mediamaid.new(params[:mediamaid])
+     respond_to do |format|
+       if @mediamaid.save
+         flash[:notice] = 'Mediamaid was successfully created.'
+         format.html { redirect_to mediamaid_url(@mediamaid) }
+         format.xml  { head :created, :location => mediamaid_url(@mediamaid) }
+         format.js do
+           responds_to_parent do
+             render :update do |page|
+               page.insert_html :bottom, "mediamaids", :partial => 'mediamaids/list_item', :object => @mediamaid
+               page.visual_effect :highlight, "mediamaid_#{@mediamaid.id}" 
+             end
+           end          
+         end
+       else
+         format.html { render :action => "new" }
+         format.xml  { render :xml => @mediamaid.errors.to_xml }
+         format.js do
+           responds_to_parent do
+             render :update do |page|
+                 # update the page with an error message
+             end
+           end          
+         end
+       end
+     end
+   end
+   
+   
+   
+ 
   
   
   def show
