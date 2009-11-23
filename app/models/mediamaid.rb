e010f29a509b220e26448e81f268ebd496bbdfde
Liah Hansen <liahhansen@gmail.com>
2009-11-23 14:44:30 -0800

    fixed image aspect ratio, changed inserted html to a div from a p

--- app/models/mediamaid.rb
+++ app/models/mediamaid.rb
@@ -1,6 +1,8 @@
 class Mediamaid < ActiveRecord::Base 
    require "paperclip"
-  has_attached_file :mediamaid, :styles => { :thumb => ["100x100#"], :small => "150x150>", :medium => "200x200>", :large => "400x400>" },   :url  => "/assets/products/:id/:style/:basename.:extension", :path => ":rails_root/public/assets/products/:id/:style/:basename.:extension"  
+  has_attached_file :mediamaid, :styles => { :thumb => "100x100>", :small => "150x150>", :medium => "200x200>", :large => "400x400>" },   :url  => "/assets/products/:id/:style/:basename.:extension", :path => ":rails_root/public/assets/products/:id/:style/:basename.:extension",  
+  # has_attached_file :avatar, :styles => { :medium => "300x300>", :small => "100x100>" },
+     :commands => { :thumb => "-background white -gravity center -extent 100x100 +repage" }
   
 
 
