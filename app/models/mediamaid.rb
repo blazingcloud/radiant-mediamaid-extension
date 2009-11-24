24a57e516e300a4d453b79e5e8556287a84e49e0
Liah Hansen <liahhansen@gmail.com>
2009-11-23 17:27:44 -0800

    more aspect ratio

--- app/models/mediamaid.rb
+++ app/models/mediamaid.rb
@@ -1,8 +1,20 @@
 class Mediamaid < ActiveRecord::Base 
    require "paperclip"
-  has_attached_file :mediamaid, :styles => { :thumb => "100x100>", :small => "150x150>", :medium => "200x200>", :large => "400x400>" },   :url  => "/assets/products/:id/:style/:basename.:extension", :path => ":rails_root/public/assets/products/:id/:style/:basename.:extension",  
-  # has_attached_file :avatar, :styles => { :medium => "300x300>", :small => "100x100>" },
-     :commands => { :thumb => "-background white -gravity center -extent 100x100 +repage" }
+  has_attached_file :mediamaid, :styles => { 
+    # :thumb => Proc.new { |instance| "#{instance.photo_width}x#{instance.photo_height}>" },
+    
+    # convert logo: -resize 80x80\> \
+      # -size 80x80 xc:blue +swap -gravity center  -composite \
+      # space_resize.jpg
+    
+    # :thumb => "-resize 100x100> -size 100x100 xc:blue +swap -gravity center  -composite ", 
+    :thumb => "100x100>",
+    :small => "150x150>", 
+    :medium => "200x200>", 
+    :large => "400x400>" },   
+    :url  => "/assets/products/:id/:style/:basename.:extension", 
+    :path => ":rails_root/public/assets/products/:id/:style/:basename.:extension", 
+    :commands => { :thumb => "-background white -gravity center -extent 100x100 +repage" }
   
 
 
