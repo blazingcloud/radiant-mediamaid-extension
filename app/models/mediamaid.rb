672621cc55ababe4df3d49ff96de9ab48a772f25
Liah Hansen <liahhansen@gmail.com>
2009-11-25 10:47:01 -0800

    fixed url in model and finished css again

--- app/models/mediamaid.rb
+++ app/models/mediamaid.rb
@@ -1,19 +1,13 @@
 class Mediamaid < ActiveRecord::Base 
-  require "paperclip"
-
+   require "paperclip"
   has_attached_file :mediamaid, :styles => { 
-      :thumb => "100x100>",
-      :small => "150x150>", 
-      :medium => "200x200>", 
-      :large => "400x400>" 
-    },   
-    :url  => "/assets/products/:id/:style/:basename.:extension", 
+    :thumb => "100x100>",
+    :small => "150x150>", 
+    :medium => "200x200>", 
+    :large => "400x400>" },   
+    :url  => "/assets/:id/:style/:basename.:extension", 
     :path => ":rails_root/public/assets/:id/:style/:basename.:extension"
-   
-   
-  validates_attachment_presence :mediamaid
-  validates_attachment_content_type :mediamaid, :content_type => 'image/jpeg'
-  validates_attachment_content_type :mediamaid, :content_type =>  'image/gif'
-  validates_attachment_content_type :mediamaid, :content_type =>  'image/png'
- 
+  
+
+
 end
