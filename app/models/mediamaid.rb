8f8ed8108ce18345ac4db41e593028375bb95567
banane <banane@gmail.com>
2009-11-24 17:13:47 -0800

    model changes to validate

--- app/models/mediamaid.rb
+++ app/models/mediamaid.rb
@@ -1,23 +1,19 @@
 class Mediamaid < ActiveRecord::Base 
-   require "paperclip"
+  require "paperclip"
 
   has_attached_file :mediamaid, :styles => { 
-    # :thumb => Proc.new { |instance| "#{instance.photo_width}x#{instance.photo_height}>" },
-    
-    # convert logo: -resize 80x80\> \
-      # -size 80x80 xc:blue +swap -gravity center  -composite \
-      # space_resize.jpg
-    
-    # :thumb => "-resize 100x100> -size 100x100 xc:blue +swap -gravity center  -composite ", 
-    :thumb => "100x100>",
-    :small => "150x150>", 
-    :medium => "200x200>", 
-    :large => "400x400>" },   
+      :thumb => "100x100>",
+      :small => "150x150>", 
+      :medium => "200x200>", 
+      :large => "400x400>" 
+    },   
     :url  => "/assets/products/:id/:style/:basename.:extension", 
-    :path => ":rails_root/public/assets/products/:id/:style/:basename.:extension", 
-    :commands => { :thumb => "-background white -gravity center -extent 100x100 +repage" }
-
-  
-
-
+    :path => ":rails_root/public/assets/:id/:style/:basename.:extension"
+   
+   
+  validates_attachment_presence :mediamaid
+  validates_attachment_content_type :mediamaid, :content_type => 'image/jpeg'
+  validates_attachment_content_type :mediamaid, :content_type =>  'image/gif'
+  validates_attachment_content_type :mediamaid, :content_type =>  'image/png'
+ 
 end
---
+++ spec/mediamaid_spec.rb
@@ -0,0 +1,8 @@
+class Mediamaid
+  def upload(files)
+  end
+
+  def score
+    0
+  end
+end
